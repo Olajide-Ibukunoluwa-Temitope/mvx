5f9076f0a75b9813597f872cbdba32f346676b86
Olajide Ibukunoluwa Temitope <ibk12mails@gmail.com>
2022-10-27 07:58:54 +0100

    finishing up with desktop view

--- src/components/SelectField/SelectField.jsx
+++ src/components/SelectField/SelectField.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./styles.css";
 
-const SelectField = ({ name, placeholder, options }) => {
+const SelectField = ({
+  name,
+  placeholder,
+  options,
+  bgColor,
+  color,
+  border,
+}) => {
   return (
     <div className="custom-container">
-      <select name={name} className="select-input">
+      <select
+        name={name}
+        className="select-input"
+        style={{ backgroundColor: bgColor, color, border }}
+      >
         <option value={""}>{placeholder}</option>
         {options.map((item, idx) => {
           return (
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import CalendarComp from "../../components/Calendar/Calendar";
 import DashboardFrame from "../../components/DashboardFrame/DashboardFrame";
 import add_square from "../../assets/icons/add_square.svg";
+import timer from "../../assets/icons/timer.svg";
+import timer_play from "../../assets/icons/timer_play.svg";
 import { eventCardData, eventTypes } from "./data";
 import EventCard from "../../components/EventCard/EventCard";
 import ModalContent from "../../components/ModalContent/ModalContent";
+import Scheduler from "react-mui-scheduler";
 import "./styles.css";
+import { getTodayDate } from "../../utilities";
+import SelectField from "../../components/SelectField/SelectField";
 
 const Dashboard = () => {
   const [date, setDate] = useState([
@@ -54,6 +59,53 @@ const Dashboard = () => {
     });
   };
 
+  const events = [
+    {
+      id: "event-1",
+      label: (
+        <div className="scheduled-event">
+          <div className="scheduled-title">
+            Accounting/Personal Finance Class{" "}
+          </div>
+          <div className="scheduled-time">8:00 AM - 9:00 AM</div>
+          <div className="icon-section">
+            <img src={timer} alt="timer icon" />
+          </div>
+        </div>
+      ),
+      groupLabel: "Dr Shaun Murphy",
+      user: "Dr Shaun Murphy",
+      color: "#f28f6a",
+      startHour: "07:00 AM",
+      endHour: "09:00 AM",
+      date: getTodayDate(),
+      createdAt: new Date(),
+      createdBy: "Kristina Mayer",
+    },
+    {
+      id: "event-1",
+      label: (
+        <div className="scheduled-event">
+          <div className="scheduled-title">
+            Accounting/Personal Finance Class{" "}
+          </div>
+          <div className="scheduled-time">8:00 AM - 9:00 AM</div>
+          <div className="icon-section">
+            <img src={timer} alt="timer icon" />
+          </div>
+        </div>
+      ),
+      groupLabel: "Dr Shaun Murphy",
+      user: "Dr Shaun Murphy",
+      color: "#f28f6a",
+      startHour: "12:00 PM",
+      endHour: "13:00 PM",
+      date: getTodayDate(),
+      createdAt: new Date(),
+      createdBy: "Kristina Mayer",
+    },
+  ];
+
   return (
     <>
       <DashboardFrame>
@@ -82,7 +134,59 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-          <div className="right-section">jnfd</div>
+          <div className="right-section">
+            <div className="scheduler-opts">
+              <div className="scheduler-opt">
+                <div className="scheduler-opt-item ">Notes</div>
+                <div className="scheduler-opt-item scheduler-opt-item-active">
+                  Timeline
+                </div>
+                <div className="scheduler-opt-item">Planner</div>
+                <div className="scheduler-opt-item">Goals</div>
+              </div>
+              <div className="tracker">
+                <div className="scheduler-opt-item opt-item">Time tracking</div>
+                <div className="scheduler-opt-item time">00:03:22</div>
+                <div>
+                  <img src={timer_play} alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="scheduler-heading">
+              <div className="scheduler">
+                <div className="scheduler-title">Calendar</div>
+                <div className="sort-scheduler">
+                  <span className="label">Sort by:</span>
+                  <SelectField
+                    name="events"
+                    bgColor={"white"}
+                    color="#6C6C6C"
+                    border="0.5px solid #D3D3D3"
+                    placeholder={"Events"}
+                    options={[
+                      { label: "Classes", value: "classes" },
+                      { label: "Reminders", value: "reminders" },
+                    ]}
+                  />
+                </div>
+              </div>
+            </div>
+            <Scheduler
+              locale="en"
+              events={events}
+              legacyStyle={false}
+              options={{
+                transitionMode: "zoom",
+                startWeekOn: "mon",
+                defaultMode: "day",
+              }}
+              toolbarProps={{
+                showSearchBar: false,
+                showSwitchModeButtons: true,
+                showDatePicker: true,
+              }}
+            />
+          </div>
         </div>
       </DashboardFrame>
       <div style={{ display: open ? "block" : "none" }}>
